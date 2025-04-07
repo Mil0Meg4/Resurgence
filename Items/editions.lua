7e315619949ce7e3d1e414694fe8f0aafd4b4916
Mil0Meg4 <milomega21gmail.com>
2025-04-07 18:39:30 +0200

    faustian added

--- Items/editions.lua
+++ Items/editions.lua
@@ -16,3 +16,21 @@ SMODS.Edition{
         end
     end
 }
+
+SMODS.Edition{
+    key = "faust",
+	order = 5,
+	shader = "faust",
+	weight = 7,
+	extra_cost = 10,
+    in_shop = true,
+	config = { dollars = 6 },
+    calculate = function(self, card, context)
+        if context.main_scoring and context.cardarea == G.play then
+            return { p_dollars = self.config.dollars }end    
+        end,
+    calc_dollar_bonus = function(self, card)
+        return self.config.dollars
+    end
+}
+

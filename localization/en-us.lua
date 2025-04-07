a2d24c5fc70d25a9428d28d26245f5151031317a
HyenaGirlFailGirl <110414444+ArchWizardLesbian@users.noreply.github.com>
2025-04-07 13:12:10 -0400

    Update en-us.lua
    added 2 more Locs
    Charon and Gabriel Apostate of Hate

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -6,13 +6,26 @@ return {
         Enhanced={},
         Joker={
             j_anva_sigma = {
-             name = 'SIGMA',
-             text = { "the next {C:purple}Tarot{} card used",
-                      "{C:attention}transforms{} this Joker into",
-                       "a {C:attention}unique{} Joker based off",
-                       "the used card"
-                    }
-                 },
+                name = 'SIGMA',
+                text = { "the next {C:purple}Tarot{} card used",
+                         "{C:attention}transforms{} this Joker into",
+                          "a {C:attention}unique{} Joker based off",
+                          "the used card"
+                       }
+                    },
+            j_anva_charon = {
+                name = 'Charon',
+                text = { "At end of shop, quintuple money",
+                         "{C:inactive}(Max of {C:money}$2000{C:inactive}){}"
+                        }
+                    },
+            j_anva_gaba = {
+                name = 'Gabriel, Apostate of Hate',
+                text = { "{X:mult,C:white}^1.25{} Mult",
+                         "At end of shop, removes {C:planet}Divine{} from",
+                         "Joker to the left to gain {X:mult,C:white}+^0.75{} Mult"
+                        }
+                    },  
             j_anva_sinclair = {
                 name = "Sinclair",
                 text = {"Earns {C:money}$#1#{} at end of round.",

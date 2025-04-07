69b921108e120b615579b8e793394a50741c4af9
blazingulag <k8lorenzo@gmail.com>
2025-04-07 17:22:58 +0200

    divine fix

--- Items/editions.lua
+++ Items/editions.lua
@@ -9,13 +9,10 @@ SMODS.Edition{
     config = {
         level = 1
     },
-    --this is so broken, wtf are you doing revo? I'll fix it tomorrow
-    --jk love u <3
     calculate = function(self,card,context)
         if context.post_joker then
-            return{
-                level_up = card.edition.level,
-            }
+            level_up_hand(card,G.GAME.last_hand_played)
+            return nil,true
         end
     end
 }

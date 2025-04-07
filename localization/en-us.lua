ee36d28b9985e206e7945b0479981c903c944c1f
HyenaGirlFailGirl <110414444+ArchWizardLesbian@users.noreply.github.com>
2025-04-07 12:04:31 -0400

    Update en-us.lua
    added SIGMA loc as well as fixed error in Sinclair loc

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -5,14 +5,22 @@ return {
         Edition={},
         Enhanced={},
         Joker={
-                j.anva.sinclair == {
+            j_anva_sigma = {
+             name = 'SIGMA',
+             text = { "the next {C:purple}Tarot{} card used",
+                      "{C:attention}transforms{} this Joker into",
+                       "a {C:attention}unique{} Joker based off",
+                       "the used card"
+                    }
+                 },
+            j_anva_sinclair = {
                 name = "Sinclair",
                 text = {"Earns {C:money}$#1#{} at end of round.",
-                        "When Shop is entered, if you have {C:money}$#1#{} or more debuffs all",
-                        "other Jokers until you have $0 or less when exiting.",
+                        "When Shop is entered, if you have {C:money}$#2#{} or more debuffs all",
+                        "other Jokers until you have {C:money}$0{} or less when exiting.",
                     },
                  }
-        },
+                    },
         Other={},
         Planet={},
         Spectral={},

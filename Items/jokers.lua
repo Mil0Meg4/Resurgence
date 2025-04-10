6749334dda442ffbefd8294e7dafa36c1d566ae5
Mil0Meg4 <milomega21gmail.com>
2025-04-10 17:02:13 +0200

    Johnatan! Charlotte! Johnatan! Charlotte! Johnatan! Charlotte! Johnatan! Charlotte!

--- Items/jokers.lua
+++ Items/jokers.lua
@@ -428,3 +428,49 @@ SMODS.Joker{
 		end
 	end
 }
+
+SMODS.Joker({
+	key = "jandc",
+	atlas = "wip",
+	pos = { x = 0, y = 0 },
+	rarity = 3,
+	cost = 10,
+	config = { extra = { chips = 50, con_slot = 8 } },
+	unlocked = true,
+	discovered = false,
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		local anv = card.ability.extra
+		return {
+			vars = { anv.chips, anv.con_slot },
+		}
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			local anv = card.ability.extra
+			local stuff = 0
+			local stuff_minus = 0
+			stuff_minus = stuff - 4
+			for i = 1, #G.consumeables.cards do
+				stuff = stuff + 1
+			end
+			if stuff_minus > 0 then
+				return {
+					chips = -stuff_minus*(anv.chips)
+				}
+		end
+	end
+end,
+
+	add_to_deck = function(self, card, from_debuff)
+			local anv = card.ability.extra
+			G.consumeables.config.card_limit = G.consumeables.config.card_limit + anv.con_slot					
+	end,
+
+	remove_from_deck = function(self, card, from_debuff)
+		local anv = card.ability.extra
+		G.consumeables.config.card_limit = G.consumeables.config.card_limit - anv.con_slot					
+	end
+
+
+})

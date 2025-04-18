c8390f0e7fbfe31130c46d11692b4d2a19e8ca33
blazingulag <k8lorenzo@gmail.com>
2025-04-19 01:43:57 +0200

    motherboard fix

--- Items/augments.lua
+++ Items/augments.lua
@@ -20,7 +20,7 @@ SMODS.Consumable({
     atlas = 'aug',
     pos = {x=0, y=0},
     discovered = true,
-    config = {max_highlighted = 1, left_val = 1,right_val = 0},
+    config = {max_highlighted = 2, left_val = 1,right_val = 0},
     loc_vars = function(self, info_queue)
         local anv = self.config
 		return { vars = {anv.max_highlighted,anv.left_val,anv.right_val} }
@@ -34,7 +34,7 @@ SMODS.Consumable({
             play_sound('tarot1')--normal consumable shenanigans
             card:juice_up(0.3, 0.5)--normal consumable shenanigans
             for _, v in ipairs(G.jokers.highlighted) do--apply to all selected jokers
-                local pos_val = G.jokers.cards[1] == v and self.config.left_val or self.config.right_val--checks if selected joker is leftmost
+                local pos_val = G.jokers.highlighted[1] == v and self.config.left_val or self.config.right_val--checks if selected joker is leftmost
                 --set all values to 1 or 0, the second argument is nil since we re not using any reference table
                 --the last one is nil too since we want to affect all values
                 if  not v.config.center.immutable then --does not apply the effect on immutable jokers
@@ -78,7 +78,7 @@ SMODS.Consumable({
     atlas = 'aug',
     pos = {x=0, y=0},
     discovered = true,
-    config = {max_highlighted = 1, tweak = "lever"},
+    config = {max_highlighted = 2, tweak = "lever"},
     loc_vars = function(self, info_queue)
         local anv = self.config
 		info_queue[#info_queue + 1] = { key = "anva_"..anv.tweak, set = "Other", vars = {} }

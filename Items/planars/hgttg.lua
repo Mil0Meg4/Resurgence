145f86ad21e81c77e52a924927c4ee094ee94556
blazingulag <k8lorenzo@gmail.com>
2025-04-25 15:35:46 +0200

    Life, Universe, and Everything

--- Items/planars/hgttg.lua
+++ Items/planars/hgttg.lua
@@ -1,3 +1,35 @@
+SMODS.Joker({
+	key = "everything",
+	atlas = "joke",
+	pos = { x = 0, y = 0 },
+	pools = {planar = true, undertale = true},
+	rarity = 4,
+	cost = 20,
+	immutable = true,
+	config = {extra = {mult = 42}},
+	unlocked = true,
+	discovered = false,
+	perishable_compat = false,
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		local anv = card.ability.extra
+		return {
+			vars = { anv.mult },
+		}
+	end,
+	calculate = function(self, card, context)
+		if context.scoring_42 or (context.cardarea == G.play and context.individual) then
+            local _card = context.other_card
+			if _card.base.value == "anva_42" then
+                local anv = card.ability.extra
+                return {
+                    mult = anv.mult
+                }
+			end
+		end
+	end,
+})
+
 local orig_create_card = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     if next(find_joker("j_anva_everything")) and _type ~= "Base" and _type ~= "Enhanced" and _type ~= "Booster" then

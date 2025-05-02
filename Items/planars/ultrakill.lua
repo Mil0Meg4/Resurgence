306c4ffc7424a0d141a9e0563fcdde79064e86ff
ArchWizardLesbian <gostman101@gmail.com>
2025-05-02 09:53:24 -0400

    buffed v1

--- Items/planars/ultrakill.lua
+++ Items/planars/ultrakill.lua
@@ -93,7 +93,7 @@ SMODS.Joker({
 SMODS.Joker {
 	key = 'v1',
 	config = { 
-	extra = {xred = 3, xredgain = 1, steelcardscorereq = 8, steelcardscored = 0} },
+	extra = {xred = 2, xredgain = 1, steelcardscorereq = 3, steelcardscored = 0} },
 	pools = {planar = true, ultrakill = true},
 	rarity = 4,
 	atlas = 'joke',
